7df042ebb784894b9e3629cf2426d60d878eb702
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2025-01-15 17:51:20 +0700

    feat(AniWayAPIGateway): добавлен модуль AniWayAPIGateway
    Родительский сервер проекта. Создан для выполнения функций маршрутизации и предоставляет единый интерфейс для клиента.

---
+++ AniWayAPIGateway/src/main/java/com/shadowshiftstudio/aniwayapigateway/AniWayApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.aniwayapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniWayApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniWayApiGatewayApplication.class, args);
+    }
+
+}
---
+++ AniWayAPIGateway/src/test/java/com/shadowshiftstudio/aniwayapigateway/AniWayApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.aniwayapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniWayApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
