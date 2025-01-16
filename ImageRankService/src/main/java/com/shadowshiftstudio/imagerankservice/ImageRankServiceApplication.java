d6c8b03a1ab7156e447d99771bfc9a10e264801e
DarlingInSteam <rombk2711@gmail.com>
2025-01-16 15:44:56 +0700

    feat(ImageRankService): добавлен модуль ImageRankService
    Микросервис ранжирования картинок

---
+++ ImageRankService/src/main/java/com/shadowshiftstudio/imagerankservice/ImageRankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.imagerankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageRankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageRankServiceApplication.class, args);
+    }
+
+}
---
+++ ImageRankService/src/test/java/com/shadowshiftstudio/imagerankservice/ImageRankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.imagerankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageRankServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
