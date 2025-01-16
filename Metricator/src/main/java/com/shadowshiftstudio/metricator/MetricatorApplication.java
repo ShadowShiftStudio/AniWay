f6d75b9cac61bd88224e5b2406bb64c4ef311007
DarlingInSteam <rombk2711@gmail.com>
2025-01-16 15:34:46 +0700

    feat(Metricator): добавлен модуль Metricator
    Микросервис сбора и записи метрик.

---
+++ Metricator/src/main/java/com/shadowshiftstudio/metricator/MetricatorApplication.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.metricator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricatorApplication.class, args);
+	}
+
+}
---
+++ Metricator/src/test/java/com/shadowshiftstudio/metricator/MetricatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.metricator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
