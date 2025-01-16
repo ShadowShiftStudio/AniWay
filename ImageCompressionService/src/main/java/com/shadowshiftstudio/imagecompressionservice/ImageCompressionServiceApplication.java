65979e49a31f5ae5155d056c6bf4dface4ef9558
DarlingInSteam <rombk2711@gmail.com>
2025-01-16 15:43:14 +0700

    feat(ImageCompressionService): добавлен модуль ImageCompressionService
    Микросервис сжатия изображений

---
+++ ImageCompressionService/src/main/java/com/shadowshiftstudio/imagecompressionservice/ImageCompressionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.imagecompressionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCompressionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageCompressionServiceApplication.class, args);
+	}
+
+}
