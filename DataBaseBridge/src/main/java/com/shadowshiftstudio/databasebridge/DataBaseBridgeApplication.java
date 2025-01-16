d11d1caecb69c8dceb3032c4144de73e176763b6
DarlingInSteam <rombk2711@gmail.com>
2025-01-16 15:38:56 +0700

    feat(DataBaseBridge): добавлен модуль DataBaseBridge
    Микросервис связи с базой данных

---
+++ DataBaseBridge/src/main/java/com/shadowshiftstudio/databasebridge/DataBaseBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.databasebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseBridgeApplication.class, args);
+	}
+
+}
---
+++ DataBaseBridge/src/test/java/com/shadowshiftstudio/databasebridge/DataBaseBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.databasebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
