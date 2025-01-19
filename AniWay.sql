8b775b9598f5ac5a60569d32780b0a26efdd29ea
DarlingInSteam <rombk2711@gmail.com>
2025-01-20 03:32:10 +0700

    add db sql script, add docker-compose for db

---
+++ AniWay.sql
@@ -0,0 +1,52 @@
+CREATE TYPE RoleEnum AS ENUM ('READER', 'TRANSLATOR', 'ADMIN', 'MODERATOR');
+
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role RoleEnum DEFAULT 'READER',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE RefreshTokens (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE BlockedUsers (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    reason TEXT
+);
+
+CREATE TABLE LoginLogs (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ip_address VARCHAR(45)
+);
+
+ALTER TABLE RefreshTokens ADD FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE;
+ALTER TABLE BlockedUsers ADD FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE;
+ALTER TABLE LoginLogs ADD FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE;
+
+-- Блок создание триггера на события изменения профиля user ------------------------------------------------------------
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_users_updated_at
+    BEFORE UPDATE ON Users
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
+------------------------------------------------------------------------------------------------------------------------
