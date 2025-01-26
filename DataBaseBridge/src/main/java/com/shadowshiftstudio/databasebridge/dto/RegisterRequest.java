6a84c8af6d508d5999c8226d8ca66c670968dd62
Radramor <Dremas2003@gmail.com>
2025-01-26 17:53:00 +0700

    feat(DataBaseBridge): add dto
    добавил несколько классов dto

---
+++ DataBaseBridge/src/main/java/com/shadowshiftstudio/databasebridge/dto/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.shadowshiftstudio.databasebridge.dto;
+
+import com.shadowshiftstudio.databasebridge.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
+}
